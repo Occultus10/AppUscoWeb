6865606397dcbd93b7d6ee7cd32dadd371972149
jgutierrez18 <jepetoto18@gmail.com>
2020-07-30 14:26:49 -0500

    pdf prueba 1

--- src/controllers/visitas.controller.js
+++ src/controllers/visitas.controller.js
@@ -7,44 +7,43 @@ const app = require('../server');
 //const { } = require("../helpers/auth");
 
 visitasCtrl.renderVisitasForm = (req, res) => {
-    res.render("visitas/nueva_visita"); //Ruta archivo .hbs
+	res.render("visitas/nueva_visita"); //Ruta archivo .hbs
 };
 
-visitasCtrl.renderVisitasTable = async(req, res) => {
-    
-    const Visitas = await Visita.find()
-    .sort({ date: "desc" })
-    .lean();
-    res.render("visitas/Visitas_visita",{ Visitas});
+visitasCtrl.renderVisitasTable = async (req, res) => {
+
+	const Visitas = await Visita.find()
+		.sort({ date: "desc" })
+		.lean();
+	res.render("visitas/Visitas_visita", { Visitas });
 };
 
 visitasCtrl.renderActualizarVisitasForm = async (req, res) => {
-   const Visitas = await Visita.findById(req.params.id).lean();
-   res.render("visitas/actualizar_visita",{Visitas});
+	const Visitas = await Visita.findById(req.params.id).lean();
+	res.render("visitas/actualizar_visita", { Visitas });
 };
 
-visitasCtrl.salidaVisitas = async(req , res) => {
+visitasCtrl.salidaVisitas = async (req, res) => {
 
-const visita = await Visita.findById(req.params.id).lean(); 
+	const visita = await Visita.findById(req.params.id).lean();
 
-const visitaSaliente = new VisitaSaliente({
-    nombres:visita.nombres,
-    cedula:visita.cedula,
-    email:visita.email,
-    telefono:visita.telefono,
-    genero:visita.genero,
-    direccion:visita.direccion,
-    temperatura:visita.temperatura,
-    lugarVisita:visita.lugarVisita,
-    nota:visita.nota,
-    Rol_visitante:visita.Rol_visitante,
-    dateEntrada:visita.date,
- }); 
- console.log(visita);  
- //await visitaSaliente.save(); 
- //await Visita.findByIdAndDelete(visita._id);
-
-//console.log('borado');
+	const visitaSaliente = new VisitaSaliente({
+		nombres: visita.nombres,
+		cedula: visita.cedula,
+		email: visita.email,
+		telefono: visita.telefono,
+		genero: visita.genero,
+		direccion: visita.direccion,
+		temperatura: visita.temperatura,
+		lugarVisita: visita.lugarVisita,
+		nota: visita.nota,
+		Rol_visitante: visita.Rol_visitante,
+		dateEntrada: visita.date,
+	});
+	console.log(visita);
+	await visitaSaliente.save();
+	await Visita.findByIdAndDelete(visita._id);
+	console.log('borado');
 
 
 
--- src/routes/pdfMake.routes.js
+++ src/routes/pdfMake.routes.js
@@ -6,6 +6,7 @@ const vfsFonts = require("../helpers/vfs_fonts");
 const { isAuthenticated } = require("../helpers/auth");
 
 const VisitaSaliente = require("../models/VisitaSaliente");
+const { text } = require('express');
 
 pdfMake.vfs = vfsFonts.pdfMake.vfs;
 
@@ -13,39 +14,100 @@ router.get("/reportes/crearReporte", isAuthenticated, (req, res) => {
     res.render('reportes/crear_reporte');
 });
 
-router.post('/reportes/crarReporte', (req, res, next) => {
+router.post('/reportes/crarReporte', async (req, res, next) => {
     //res.send('PDF');
 
-    const Visitas = VisitaSaliente.find()
+
+
+    /* const Visitas = VisitaSaliente.find({}, function (err, data) {
+         console.log('Errors: '+ err, 'Data length: ' + data.length);
+         const reportes = data;
+         //console.log('Reportes : ', reportes);
+         return reportes; 
+     });
+     console.log('Reportes ',Visitas); 
+         {text:'Cedula'},
+         {text:'Telefono'},
+         {text:'Email'},
+         {text:'Genero'},
+         {text:'Lugar de Visita'},
+         {text:'Hora entrada'},
+         {text:'Hora salida'},*/
+
+    const Visitas = VisitaSaliente.find();
+    const reportes = [];
+    for await (const doc of Visitas) {
+        reportes.push(doc);
+    }
     
-    const lname = req.body.lname;
+    console.log('reportesArray: ', reportes.length);
+    
+    const pdfdata =[]; 
+    pdfdata.push([
+        {text:'Nombres'},
+        {text:'Cedula'},
+        {text:'Telefono'},
+    ]);
 
-    var documentDefinition = {
-        content: [
-            { text: 'Tables', style: 'header' },
-            'Official documentation is in progress, this document is just a glimpse of what is possible with pdfmake and its layout engine.',
-            { text: 'A simple table (no headers, no width specified, no spans, no styling)', style: 'subheader' },
-            'The following table has nothing more than a body array',
-            {
-                style: 'tableExample',
-                table: {
-                    body: Visitas
-                }
-            }
-        ]
-    };
+    for(i = 0; i < reportes.length; i++) {
+        console.log('registro ', i);
+        pdfdata.push([
+            {text: reportes[i].nombres},
+            {text: reportes[i].cedula},
+            {text: reportes[i].telefono}
+        ]);
+        console.log('fila: '+ i , pdfdata);
+    }
+    console.log(pdfdata.length); 
 
-    const pdfDoc = pdfMake.createPdf(documentDefinition);
-    pdfDoc.getBase64((data) => {
-        res.writeHead(200,
-            {
-                'Content-Type': 'application/pdf',
-                'Content-Disposition': 'attachment;filename="filename.pdf"'
-            });
 
-        const download = Buffer.from(data.toString('utf-8'), 'base64');
-        res.end(download);
-    });
+
+
+    //pdfdata.push([{text: reportes[0].nota}],[{text:reportes[4].nota}]); 
+    // console.log(pdfdata[2]);
+
+
+    /*
+    const pdfdata =[]; 
+    
+          pdfdata.push([
+            {text:'Nombres'},
+            {text:'Cedula'},
+            {text:'Telefono'},
+            {text:'Email'},
+            {text:'Genero'},
+            {text:'Temperatura'},
+            {text:'Lugar de Visita'},
+            {text:'Hora entrada'},
+            {text:'Hora salida'}
+          ]);
+           */
+         var documentDefinition = {
+              content: [
+                  { text: 'Tables', style: 'header' },
+                  'Official documentation is in progress, this document is just a glimpse of what is possible with pdfmake and its layout engine.',
+                  { text: 'A simple table (no headers, no width specified, no spans, no styling)', style: 'subheader' },
+                  'The following table has nothing more than a body array',
+                  {
+                      style: 'tableExample',
+                      table: {
+                          body: pdfdata
+                      }
+                  }
+              ]
+          };
+      
+          const pdfDoc = pdfMake.createPdf(documentDefinition);
+          pdfDoc.getBase64((data) => {
+              res.writeHead(200,
+                  {
+                      'Content-Type': 'application/pdf',
+                      'Content-Disposition': 'attachment;filename="filename.pdf"'
+                  });
+      
+              const download = Buffer.from(data.toString('utf-8'), 'base64');
+              res.end(download);
+          });
 });
 
 
